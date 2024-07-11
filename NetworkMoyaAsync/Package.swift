ac8830781b6fcc2c8dd68be0d016d9723f2f7485
Nouman Gul <Noumanguljunejo@gmail.com>
2024-07-12 01:47:37 +0400

    added Moya networking

--- NetworkMoyaAsync/Package.swift
+++ NetworkMoyaAsync/Package.swift
@@ -5,17 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "NetworkMoyaAsync",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NetworkMoyaAsync",
             targets: ["NetworkMoyaAsync"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/Moya/Moya.git", from: "15.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NetworkMoyaAsync"),
+            name: "NetworkMoyaAsync",
+            dependencies: [
+                .product(name: "Moya", package: "Moya")
+            ]),
         .testTarget(
             name: "NetworkMoyaAsyncTests",
             dependencies: ["NetworkMoyaAsync"]),
