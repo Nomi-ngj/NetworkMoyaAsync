fb176ea56b2f5357247c387340ce9ba04d45c338
Nouman Gul <Noumanguljunejo@gmail.com>
2024-07-12 02:10:02 +0400

    adding network constant

---
+++ NetworkMoyaAsync/Sources/NetworkMoyaAsync/Base/NetworkConstants.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkConstants.swift
+//
+//
+//  Created by Nouman Gul Junejo on 12/07/2024.
+//
+
+import Foundation
+
+struct NetworkConstants{
+
+    static let baseURL = URL(string: "https://reqres.in/api")! //complete url = /users?page=1
+    
+}
--- NetworkMoyaAsync/Sources/NetworkMoyaAsync/NetworkMoyaAsync.swift
+++ NetworkMoyaAsync/Sources/NetworkMoyaAsync/NetworkMoyaAsync.swift
@@ -1,2 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+public class NetworkMoyaAsync{
+    public init(){}
+    
+    
+}
