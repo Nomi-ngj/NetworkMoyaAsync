8fdff29f7d0dafafe5844be87fc63b19b9a7b12f
Nouman Gul <Noumanguljunejo@gmail.com>
2024-07-12 04:56:47 +0400

    adding unit testing for user client

--- Tests/NetworkMoyaAsyncTests/NetworkMoyaAsyncTests.swift
+++ Tests/NetworkMoyaAsyncTests/NetworkMoyaAsyncTests.swift
@@ -2,11 +2,28 @@ import XCTest
 @testable import NetworkMoyaAsync
 
 final class NetworkMoyaAsyncTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    func testGetUsers() async throws {
+        // Arrange
+        let sut = UserClient()
+        let request = UserRequest(page: 1)
+        let expectedUsersCount = 6 // Adjust based on your response data
+        
+        // Act
+        let users = try await sut.getUsers(request: request)
+        // Additional assertions if you want to validate individual user details
+        if let users = users.data {
+            // Assert
+            XCTAssertEqual(users.count, expectedUsersCount, "Number of received users should match expected count")
+            
+            // Example assertion for the first user's details
+            XCTAssertEqual(users[0].id, 1, "First user ID should match expected value")
+            XCTAssertEqual(users[0].email, "george.bluth@reqres.in", "First user email should match expected value")
+            XCTAssertEqual(users[0].firstName, "George", "First user first name should match expected value")
+            XCTAssertEqual(users[0].lastName, "Bluth", "First user last name should match expected value")
+            XCTAssertEqual(users[0].avatar, "https://reqres.in/img/faces/1-image.jpg", "First user avatar URL should match expected value")
+        } else {
+            XCTFail("Pagination data is nil")
+        }
     }
 }
